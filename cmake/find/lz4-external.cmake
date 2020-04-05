cdf0fcecff90cba988ee49db77680649d55fe0d5
msaf1980 <msaf1980@gmail.com>
2020-04-05 23:07:17 +0500

    some refactor

--- cmake/find/lz4-external.cmake
+++ cmake/find/lz4-external.cmake
@@ -1,9 +1,9 @@
 option (USE_LZ4 "Set to TRUE to use lz4 library" OFF)
-option (SYSTEM_LZ4_LIBRARY "Set to TRUE to use lz4 library ${LZ_VERSION} instead of system" OFF)
+option (USE_SYSTEM_LZ4 "Set to TRUE to use system lz4 library instead of external ${LZ_VERSION}" OFF)
 
 if (USE_LZ4)
 	if (SYSTEM_LZ4_LIBRARY)
-		find_library( LZ4_LIBRARY lz4 )
+		find_library( LZ4_LIBRARY lz4)
 		find_path( LZ4_INCLUDE_DIR NAMES lz4.h )
 	else(SYSTEM_LZ4_LIBRARY)
 		ExternalProject_Add( lz4
@@ -13,11 +13,14 @@ if (USE_LZ4)
 			CONFIGURE_COMMAND ""
 			BUILD_COMMAND make
 			BUILD_IN_SOURCE 1
-			INSTALL_COMMAND ""
+			TEST_COMMAND ""
+			INSTALL_COMMAND rm -f ${DEP_INSTALL_DIR}/liblz4.* ; make install PREFIX=${DEP_INSTALL_DIR}
 		)
 		ExternalProject_Get_Property( lz4 binary_dir )
-		set( LZ4_LIBRARY "${binary_dir}/lib/liblz4.a" )
-		set( LZ4_INCLUDE_DIR NAMES "${binary_dir}/lib" )
+		set( LZ4_LIBRARY "${DEP_INSTALL_DIR}/lib/liblz4.a" )
+		set( LZ4_INCLUDE_DIR NAMES "${DEP_INSTALL_DIR}/include" )
+		#set( LZ4_LIBRARY "${binary_dir}/lib/liblz4.a" )
+		#set( LZ4_INCLUDE_DIR NAMES "${binary_dir}/lib" )
 	endif ()
 
 	message( STATUS "Using lz4: ${LZ4_INCLUDE_DIR} : ${LZ4_LIBRARY}" )
